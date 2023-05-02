6cd201c87236d8e323710e841910a87f721c9b6c
B4TARA <evgeniybaturel@gmail.com>
2023-05-02 12:30:22 +0300

    new

---
+++ model/methods.js
@@ -0,0 +1,78 @@
+const fs = require("fs");
+
+let cellPhones = require("./cellphones");
+
+module.exports = {
+  getPhones: () => cellPhones,
+
+  getPhoneById: (id) => cellPhones.find((phone) => phone.id === Number(id)),
+
+  addPhone(fields) {
+    try {
+      const { fullName, phone } = fields;
+      if (!fullName || !phone) {
+        throw new Error("Empty fullName or phone fields");
+      }
+
+      if (cellPhones.find((phone) => phone.fullName === fullName)) {
+        throw new Error("Duplicate fullName");
+      }
+
+      const newPhone = {
+        id: cellPhones.length,
+        fullName,
+        phone,
+      };
+      cellPhones.push(newPhone);
+      save();
+      return newPhone;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  updatePhone(fields) {
+    try {
+      const { id, fullName, phone } = fields;
+      if (!id || !fullName || !phone) {
+        throw new Error("Empty id, fullName or phone fields");
+      }
+      let targetPhone = cellPhones.find((phone) => phone.id === Number(id));
+      if (!targetPhone) {
+        throw new Error("Invalid record id");
+      }
+      targetPhone.fullName = fullName;
+      targetPhone.phone = phone;
+      save();
+      return targetPhone;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  deletePhone(id) {
+    try {
+      let targetPhone = cellPhones.find((phone) => phone.id !== Number(id));
+      if (!targetPhone) {
+        throw new Error("Invalid record id");
+      }
+      cellPhones = cellPhones.filter((phone) => phone.id !== Number(id));
+      save();
+      return targetPhone;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
+
+function save() {
+  fs.writeFile(
+    "./model/cellphones.json",
+    JSON.stringify(cellPhones, null, "  "),
+    (err) => {
+      if (err) {
+        throw err;
+      }
+    }
+  );
+}
